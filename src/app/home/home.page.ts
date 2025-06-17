5f5f47fcf77ed2776ba4018fc79e9c45812f0a7f
Marcos Henrique Araújo <mine.rique.games@gmail.com>
2025-06-17 10:21:26 -0300

    feat: updates the project structure with new routes, homepage improvements and integration with the Pokémon API

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { forkJoin } from 'rxjs';
 
 @Component({
   selector: 'app-home',
@@ -6,8 +9,49 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
   standalone: false,
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  pokemons: any[] = [];
+  offset = 0;
+  limit = 20;
+  loading = false;
 
-  constructor() {}
+  constructor(private http: HttpClient, private router: Router) {}
 
+  ngOnInit() {
+    this.loadPokemons();
+  }
+
+  loadPokemons() {
+    this.loading = true;
+    this.http
+      .get(
+        `https://pokeapi.co/api/v2/pokemon?limit=${this.limit}&offset=${this.offset}`
+      )
+      .subscribe((data: any) => {
+        const pokemonRequests = data.results.map((pokemon: any) =>
+          this.http.get(pokemon.url)
+        );
+
+        forkJoin(pokemonRequests).subscribe((pokemonDetails: any) => {
+          const pokemonsWithImages = pokemonDetails.map((pokemon: any) => ({
+            name: pokemon.name,
+            url: `https://pokeapi.co/api/v2/pokemon/${pokemon.id}`,
+            id: pokemon.id,
+            image:
+              pokemon.sprites.other['official-artwork'].front_default ||
+              pokemon.sprites.front_default ||
+              'assets/icon/favicon.png',
+          }));
+
+          this.pokemons = this.pokemons.concat(pokemonsWithImages);
+          this.offset += this.limit;
+          this.loading = false;
+        });
+      });
+  }
+
+  goToDetails(pokemonUrl: string) {
+    const id = pokemonUrl.split('/').filter(Boolean).pop();
+    this.router.navigate(['/details', id]);
+  }
 }
